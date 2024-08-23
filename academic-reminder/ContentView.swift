a3244ffbbd6ecd86c4db13fb54c52783941f3731
Julia Ishibashi <juliaishibashi@uvic.ca>
2024-08-23 12:49:56 -0700

    impliment interface

--- academic-reminder/ContentView.swift
+++ academic-reminder/ContentView.swift
@@ -1,24 +1,40 @@
 //
 //  ContentView.swift
-//  academic-reminder
+//  academic_reminder
 //
-//  Created by Julia on 2024-08-23.
+//  Created by Julia on 2024-08-21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Home()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            AssignmentsView()
+                .tabItem {
+                    Label("Assignments", systemImage: "book.pages")
+                }
+            
+            GradesView()
+                .tabItem {
+                    Label("Grades", systemImage: "checkmark.square.fill")
+                }
+            CoursesView()
+                .tabItem {
+                    Label("Courses", systemImage: "graduationcap")
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
