6cee0f01f5a92c7e2d8a1ccb80020528015d7781
Julia Ishibashi <julia.ish12@gmail.com>
2024-11-26 02:26:47 -0800

    Push notification implementation (not complete)

--- academic-reminder/academic_reminderApp.swift
+++ academic-reminder/academic_reminderApp.swift
@@ -1,15 +1,47 @@
+////
+////  academic_reminderApp.swift
+////  academic-reminder
+////
+////  Created by Julia on 2024-08-23.
+////
+//import SwiftUI
+//import UserNotifications
+//import SwiftData
 //
-//  academic_reminderApp.swift
-//  academic-reminder
+//@main
+//struct academic_reminderApp: App {
 //
-//  Created by Julia on 2024-08-23.
-//
+//    init() {
+//        requestNotificationPermission()
+//    }
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .modelContainer(for: Assignment.self)
+//        }
+//    }
+//    
+//    private func requestNotificationPermission() {
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+//            if granted {
+//                print("notification allowed")
+//            } else {
+//                print("notification dened")
+//            }
+//        }
+//    }
+//}
 
 import SwiftUI
 import SwiftData
+import UserNotifications
 
 @main
 struct academic_reminderApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
